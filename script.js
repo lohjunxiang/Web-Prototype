c4084a1675707fb9cddef484ad8a0eaaf25a0812
Jun <jloh3956@uni.sydney.edu.au>
2020-11-27 19:13:43 +0800

    finished footer

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 var infos = [
     'Because of government regulations, we may be unable to accept white goods/electrical items and mattresses.',
     'It costs The Salvation Army millions of dollars each year to remove rubbish that has been dumped outside Salvos Stores.',
-    'We cannot accept things like Children\'s car seats, booster seats and baby capsules without the presence of the Standards Australia logo "AS/NZS 1754-2010"'
+    'We cannot accept things like Children\'s car seats, booster seats and baby capsules without the presence of the Standards Australia logo "AS/NZS 1754-2010"',
+    'We cannot accept any computer monitors, printers, scanners and other hardware, unless they are brand new and still in packaging.'
 ]
 
 function newInfo() {
@@ -10,4 +11,5 @@ function newInfo() {
     console.log(randomNumber)
 }
 
-setInterval(newInfo, 1000);
+setTimeout(newInfo, 100);
+setInterval(newInfo, 10000);
