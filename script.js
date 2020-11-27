15fe6837f9463ee162b4832e18cbfebee4f3ef92
Jun <jloh3956@uni.sydney.edu.au>
2020-11-27 13:46:15 +0800

    problems with setTimer function to refresh info

---
+++ script.js
@@ -0,0 +1,16 @@
+var infos = [
+    'Because of government regulations, we may be unable to accept white goods/electrical items and mattresses.',
+    'It costs The Salvation Army millions of dollars each year to remove rubbish that has been dumped outside Salvos Stores.',
+    'We cannot accept things like Children\'s car seats, booster seats and baby capsules without the presence of the Standards Australia logo "AS/NZS 1754-2010"'
+]
+
+
+setTimeout(() => console.log("hi"), 1000);
+setTimeout(newInfo, 1000);
+
+function newInfo() {
+    var randomNumber = Math.floor(Math.random() * (infos.length));
+    document.getElementById('randomInfo').innerHTML = infos[randomNumber];
+    console.log(randomNumber)
+}
+
